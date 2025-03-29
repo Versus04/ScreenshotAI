02b7070195153ee868a2d4c7497fbf1d91ee6c90
Versus04 <2shubhambind@gmail.com>
2025-03-30 00:09:44 +0530

    base files and dependencies

---
+++ app/src/main/java/com/example/screenshotai/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.screenshotai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.screenshotai.ui.theme.ScreenshotaiTheme
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScreenshotaiTheme {
+               inputing()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun inputing()
+{
+    Column(Modifier.fillMaxSize() , verticalArrangement = Arrangement.Center ,
+        horizontalAlignment = Alignment.CenterHorizontally ){
+            Text("Home Screen")
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+        ) {
+            Icon(imageVector = Icons.Default.Home , contentDescription = null)
+        }
+    }
+}
+
+fun recognize()
+{
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+}
